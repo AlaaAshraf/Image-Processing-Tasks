2a0d031a625d760c9e91ae7a0d565eef2453da18
Ayman Zaki <ayman.zaki.az@gmail.com>
2016-11-28 05:37:06 +0200

    Detect & Count flowers

--- SegmentFlowers.m
+++ SegmentFlowers.m
@@ -1,9 +1,23 @@
 function [NumberOfFlowers, NumberOfPetalsPerFlower] = SegmentFlowers(InputImage)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-    bw = im2bw(InputImage, 0.5);
-    figure, imshow(bw);
-    NumberOfFlowers = 5;
+    NumberOfFlowers = 0;
+    h = fspecial('gaussian', [3 3], 2);
+    InputImage = imfilter(InputImage,h);
+    bw = im2bw(InputImage, 0.37);
+    tmp = bw;
+    CC = bwconncomp(bw);
+    numPixels = cellfun(@numel,CC.PixelIdxList);
+    [L W] = size(bw);
+    for ind = 1: CC.NumObjects
+       if(numPixels(1, ind) >= (L*W)/20)
+           NumberOfFlowers = NumberOfFlowers + 1;
+           tmp(CC.PixelIdxList{ind}) = 0;
+       end
+    end
+    flowers = bw - tmp;
+    
+    figure, imshow(flowers);
     NumberOfPetalsPerFlower = 5;
+    
 end
-
